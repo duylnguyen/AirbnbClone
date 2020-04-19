19939eaad87f8e6b02f4af4c2925c969a3cd9514
Duy Nguyen <duy_nobita@icloud.com>
2020-04-19 10:08:12 -0400

    Create Login screen

--- src/screens/LogIn.js
+++ src/screens/LogIn.js
@@ -9,15 +9,42 @@ import {
   KeyboardAvoidingView,
 } from 'react-native';
 import colors from '../styles/colors';
+import InputField from '../components/form/InputField';
+import NextArrowButton from '../components/buttons/NextArrowButton';
 
 export default class Login extends Component {
+  handleNextButton() {
+    alert('Next Button Pressed');
+  }
+
   render() {
     return (
       <KeyboardAvoidingView style={styles.wrapper}>
         <View style={styles.scrollViewWrapper}>
           <ScrollView style={styles.scrollView}>
             <Text style={styles.loginHeader}>Log In</Text>
+            <InputField
+              labelText="EMAIL ADDRESS"
+              labelTextSize={14}
+              labelTextColor={colors.white}
+              textColor={colors.white}
+              borderBottomColor={colors.white}
+              inputType="email"
+              customStyle={{marginBottom: 30}}
+            />
+            <InputField
+              labelText="PASSWORD"
+              labelTextSize={14}
+              labelTextColor={colors.white}
+              textColor={colors.white}
+              borderBottomColor={colors.white}
+              inputType="password"
+              customStyle={{marginBottom: 30}}
+            />
           </ScrollView>
+        </View>
+        <View style={styles.nextButton}>
+          <NextArrowButton handeNextButton={this.handleNextButton} />
         </View>
       </KeyboardAvoidingView>
     );
@@ -45,5 +72,10 @@ const styles = StyleSheet.create({
     color: colors.white,
     fontWeight: '300',
     marginBottom: 40,
+  },
+  nextButton: {
+    alignItems: 'flex-end',
+    right: 20,
+    bottom: 20,
   },
 });
